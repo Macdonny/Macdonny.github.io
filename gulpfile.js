2463a23d3331d4e51725b94adf43462a987543a1
Macdonny Saintil <macdonnysaintil@hotmail.com>
2016-07-04 17:32:16 -0400

    Add gulp to automate sass compliation.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+gulp.task('sass', function () {
+  return gulp.src(['./bower_components/Materialize/sass/\**/\*.scss', './_sass/\**/\*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./_maps'))
+    .pipe(gulp.dest('./_css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch(['./bower_components/Materialize/sass/\**/*.scss', './_sass/\**/\*.scss'], ['sass']);
+});
